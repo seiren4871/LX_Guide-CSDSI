abeddfae6849105430e8c850de1451c5676ec9ec
Natthida Thammasorn <mai.nutthida127@gmail.com>
2020-05-12 03:15:04 +0700

    Commit

---
+++ lib/contact.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'drawer.dart';
+
+class ContactUs extends StatefulWidget {
+  @override
+  _ContactUsState createState() => _ContactUsState();
+}
+
+enum ConfirmAction { CANCEL, ACCEPT }
+
+Future<ConfirmAction> _asyncConfirmDialog(BuildContext context) async {
+  return showDialog<ConfirmAction>(
+    context: context,
+    barrierDismissible: false, // user must tap button for close dialog!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Are you sure?'),
+        content: const Text('This will not back to fix it again.'),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text('CANCEL'),
+            onPressed: () {
+              Navigator.of(context).pop(ConfirmAction.CANCEL);
+            },
+          ),
+          FlatButton(
+            child: const Text('ACCEPT'),
+            onPressed: () {
+              Navigator.of(context).pop(ConfirmAction.ACCEPT);
+              MaterialPageRoute materialPageRoute =
+                  MaterialPageRoute(builder: (BuildContext context) => MyApp());
+              Navigator.of(context).push(materialPageRoute);
+            },
+          )
+        ],
+      );
+    },
+  );
+}
+
+class _ContactUsState extends State<ContactUs> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFf8777c),
+        title: Text("Contact Us"),
+      ),
+      body: new SafeArea(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/img/bg.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xffffffff).withOpacity(0.6),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            padding: new EdgeInsets.all(10.0),
+            margin: EdgeInsets.fromLTRB(15.0, 80.0, 15.0, 150.0),
+            child: Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.feedback),
+                  color: Color(0xFFf8777c),
+                  iconSize: 80.0,
+                  onPressed: null,
+                ),
+                Text(
+                  "Contact Us",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color:  Color(0xFFf8777c),
+                      fontSize: 20.0),
+                ),
+                nameTag(),
+                emailTag(),
+                messageTag(),
+                submitButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Back');
+          MaterialPageRoute materialPageRoute =
+              MaterialPageRoute(builder: (BuildContext context) => MyApp());
+          Navigator.of(context).push(materialPageRoute);
+        },
+        tooltip: 'back',
+        child: Icon(Icons.arrow_back),
+        foregroundColor: Color(0xFF0e1b47),
+        backgroundColor: Color(0xFF66bcc0),
+        mini: true,
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text(
+        'Send',
+        style: TextStyle(color: Color(0xff214C7D)),
+      ),
+      color: Color(0xff64bcbe),
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(50.0),
+      ),
+      onPressed: () {
+        print('Send');
+        _asyncConfirmDialog(context);
+      },
+    );
+  }
+
+  final backgroundImage = new Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("assets/img/bg.jpg"),
+        fit: BoxFit.cover,
+      ),
+    ),
+  );
+
+  Widget messageTag() {
+    return new Container(
+      height: 100.0,
+      margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+      child: new ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: 200.0,
+        ),
+        child: TextField(
+          enabled: true,
+          textInputAction: TextInputAction.newline,
+          keyboardType: TextInputType.multiline,
+          maxLines: 7,
+          style: new TextStyle(
+            color: Color(0xff214C7D),
+          ),
+          decoration: new InputDecoration(
+            fillColor: Color(0xff64bcbe).withOpacity(0.6),
+            filled: true,
+            hintText: 'Write your message here',
+            contentPadding: EdgeInsets.all(10.0),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide(color: Color(00000000))),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget nameTag() {
+    return new Container(
+      height: 50.0,
+      margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+      child: TextField(
+        enabled: true,
+        textInputAction: TextInputAction.newline,
+        keyboardType: TextInputType.text,
+        maxLines: null,
+        style: new TextStyle(
+          color: Color(0xff214C7D),
+        ),
+        decoration: new InputDecoration(
+          fillColor: Color(0xff64bcbe).withOpacity(0.6),
+          filled: true,
+          hintText: 'Write your name here',
+          contentPadding: EdgeInsets.all(15.0),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+              borderSide: BorderSide(color: Color(00000000))),
+        ),
+      ),
+    );
+  }
+
+  Widget emailTag() {
+    return new Container(
+      height: 50.0,
+      margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+      child: TextField(
+        enabled: true,
+        textInputAction: TextInputAction.newline,
+        keyboardType: TextInputType.emailAddress,
+        maxLines: null,
+        style: new TextStyle(
+          color: Color(0xff214C7D),
+        ),
+        decoration: new InputDecoration(
+          fillColor: Color(0xff64bcbe).withOpacity(0.6),
+          filled: true,
+          hintText: 'Write your email here',
+          contentPadding: EdgeInsets.all(15.0),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+              borderSide: BorderSide(color: Color(00000000))),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:lx_guide/informationPage.dart';
+import 'package:lx_guide/main_IndoorMap.dart';
+import 'package:flutter/services.dart';
+import 'contact.dart';
+import 'theme.dart';
+
+final ThemeData _colorTheme = _buildColorTheme();
+ThemeData _buildColorTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    accentColor: color50,
+    primaryColor: color300,
+    scaffoldBackgroundColor: BackgroundWhite,
+    cardColor: BackgroundWhite,
+    textSelectionColor: color100,
+    errorColor: ErrorRed,
+  );
+}
 
 void main() => runApp(MyApp());
 
@@ -8,18 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: _colorTheme,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -28,15 +35,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,67 +42,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text(
+        'Contact',
+        style: TextStyle(color: Color(0xff000000)),
+      ),
+      color: Color(0xff64bcbe),
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(18.0),
+      ),
+      onPressed: () {
+        print('Contact');
+        MaterialPageRoute materialPageRoute =
+            MaterialPageRoute(builder: (BuildContext context) => ContactUs());
+        Navigator.of(context).push(materialPageRoute);
+      },
+    );
+  }
+
+  Widget mapButton() {
+    return RaisedButton(
+      child: Text(
+        'Indoor Map',
+        style: TextStyle(color: Color(0xff000000)),
+      ),
+      color: Color(0xff64bcbe),
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(18.0),
+      ),
+      onPressed: () {
+        print('Go to Map');
+        MaterialPageRoute materialPageRoute =
+            MaterialPageRoute(builder: (BuildContext context) => IndoorMap());
+        Navigator.of(context).push(materialPageRoute);
+      },
+    );
+  }
+
+  Widget informationButton() {
+    return RaisedButton(
+      child: Text(
+        'About',
+        style: TextStyle(color: Color(0xff000000)),
+      ),
+      color: Color(0xff64bcbe),
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(18.0),
+      ),
+      onPressed: () {
+        print('Go to Map');
+        MaterialPageRoute materialPageRoute =
+            MaterialPageRoute(builder: (BuildContext context) => InformationPage());
+        Navigator.of(context).push(materialPageRoute);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'TEST',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            submitButton(),
+            mapButton(),
+            informationButton(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: null,
+        tooltip: 'back',
+        child: Icon(Icons.arrow_back),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
